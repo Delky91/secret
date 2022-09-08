9da7babce8d1c1dc760c3fd9b62249c41189a09a
Luis Miño <l.mino.bustos@gmail.com>
2022-09-08 17:41:01 -0300

    security level 4 bcrypt and salting

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejs = require('ejs');
 const mongoose = require("mongoose");
 const { appendFile } = require("fs");
 const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -35,11 +37,14 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
     });
-    newUser.save((err) => { //cuando se usa el save automaticamente se encriptara la password en una linea binaria dificil de decifrar
+
+    newUser.save((err) => {
         if (err)
             console.log(err);
         else
@@ -49,8 +54,8 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
-    console.log(password);
+    const password = req.body.password;
+
 
     User.findOne({ email: username }, (err, foundUser) => {
         if (err)
@@ -58,9 +63,11 @@ app.post("/login", (req, res) => {
         else {
             if (foundUser) {
                 if (foundUser.password === password)
-                    res.render("secrets");
-                else
-                    res.render("the password is wrong");
+                    bcrypt.compare(password, foundUser.password, (err, result) => {
+                        if (result === true) {
+                            res.render("/secrets");
+                        }
+                    });
             }
         }
     });
