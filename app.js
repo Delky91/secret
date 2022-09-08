891da13e92299a7366941e731bfcec93d393f662
Luis Miño <l.mino.bustos@gmail.com>
2022-09-08 17:50:44 -0300

    security level 4 bcrypt and salting

--- app.js
+++ app.js
@@ -53,9 +53,9 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
+
     const username = req.body.username;
     const password = req.body.password;
-
 
     User.findOne({ email: username }, (err, foundUser) => {
         if (err)
@@ -75,5 +75,4 @@ app.post("/login", (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server up, PORT 3000');
-
 });
